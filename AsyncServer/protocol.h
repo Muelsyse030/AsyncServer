47c6ff55986d5633410c7af531061e56dac55542
Muelsyse030 <15279290607@163.com>
2025-10-18 16:39:02 +0800

    000

--- AsyncServer/protocol.h
+++ AsyncServer/protocol.h
@@ -1,13 +1,13 @@
 #pragma once
 #include <cstdint>
-
+#pragma pack(push, 1)
 // 消息协议相关的常量定义
 constexpr size_t MAX_BODY_LENGTH = 1024 * 1024;  // 1MB
 
-#pragma pack(push, 1)
+
 
 // 消息头定义
 struct MsgHeader {
-    uint16_t msg_id;     // 消息类型
-    uint32_t body_len;   // 正文字节长度（不含头）
+	uint16_t msg_id;     // 消息类型
+	uint32_t body_len;   // 正文字节长度（不含头）
 };
